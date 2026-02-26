a4846826b11a7ee16dd3ed366cbb032ca14786b4
Arham-Qureshi <arhamqureshi0098@gmail.com>
2026-02-26 16:00:51 +0530

    commit

--- app.py
+++ app.py
@@ -1,31 +1,7 @@
-from flask import Flask, render_template, request, jsonify, Response
+from flask import Flask, render_template, request, jsonify
 import scheduler
 
 app = Flask(__name__)
-BASE_URL = 'https://algo-wars.onrender.com'
-
-
-@app.route('/robots.txt')
-def robots():
-    txt = f"""User-agent: *
-Allow: /
-
-Sitemap: {BASE_URL}/sitemap.xml
-"""
-    return Response(txt, mimetype='text/plain')
-
-
-@app.route('/sitemap.xml')
-def sitemap():
-    xml = f"""<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>{BASE_URL}/</loc>
-    <changefreq>monthly</changefreq>
-    <priority>1.0</priority>
-  </url>
-</urlset>"""
-    return Response(xml, mimetype='application/xml')
 
 
 @app.route('/')
